9c03fe650d59fa700363a988de0ef8be46f68fe9
JamesMeEE <wongchapat.james@gmail.com>
2026-04-02 01:38:26 +0700

    a

--- js/livereport.js
+++ js/livereport.js
@@ -117,6 +117,13 @@ function lrParseDate(dateVal) {
       }
       return new Date(year, month, day);
     }
+    if (s.includes('T') && s.includes('Z')) {
+      var utc = new Date(s);
+      if (!isNaN(utc.getTime())) {
+        var local = new Date(utc.getTime() + 7 * 60 * 60000);
+        return new Date(local.getFullYear(), local.getMonth(), local.getDate(), local.getHours(), local.getMinutes(), local.getSeconds());
+      }
+    }
     var isoMatch = s.match(/^(\d{4})-(\d{2})-(\d{2})/);
     if (isoMatch) {
       return new Date(parseInt(isoMatch[1]), parseInt(isoMatch[2]) - 1, parseInt(isoMatch[3]));
